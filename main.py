9a1a5dea8a4ee2058e04328f3e24ee9abf703912
MovingJu <baramishe@gmail.com>
2025-03-13 12:47:39 +0900

    form, result: session update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'admin'
 
 @app.route('/')
 def index():
@@ -8,11 +9,28 @@ def index():
 
 @app.route('/about')
 def hello():
+    print('user got in about')
     return render_template("about.html")
 
-@app.route('/form')
+@app.route('/form', methods=["GET", "POST"])
 def form():
+    if request.method == "POST":
+        print("user input detected!")
+        session['user_name'] = request.form.get("first_name") # + request.form.get('last_name')
+        session['user_subscription'] = "Yes" if request.form.get("subscribe") else "No"
+        return redirect(url_for('result'))
     return render_template("form.html")
+
+@app.route('/result')
+def result():
+    name = session.get('user_name','Guest')
+    subscribe = session.get('user_subscription', 'No') 
+    return render_template("result.html", name=name, subscribe=subscribe)
+
+@app.route('/clear')
+def clear():
+    session.clear()
+    return redirect(url_for('form'))
 
 if __name__ == '__main__':
     app.run(debug=True)
